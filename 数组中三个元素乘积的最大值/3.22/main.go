ea4248fa22dc26d44fee1fc6e4aa663e7b61cdc6
sky-bai <569531262@qq.com>
2022-03-23 10:28:41 +0800

    数组中三个元素乘积的最大值

---
+++ 数组中三个元素乘积的最大值/3.22/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "math"
+
+func maximumProduct(nums []int) int {
+	// 最小的和第二小的
+	min1, min2 := math.MaxInt64, math.MaxInt64
+	// 最大的、第二大的和第三大的
+	max1, max2, max3 := math.MinInt64, math.MinInt64, math.MinInt64
+
+	for _, x := range nums {
+		if x < min1 {
+			min2 = min1
+			min1 = x
+		} else if x < min2 {
+			min2 = x
+		}
+
+		if x > max1 {
+			max3 = max2
+			max2 = max1
+			max1 = x
+		} else if x > max2 {
+			max3 = max2
+			max2 = x
+		} else if x > max3 {
+			max3 = x
+		}
+	}
+
+	return max(min1*min2*max1, max1*max2*max3)
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
